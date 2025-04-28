99e921159c7ba707a97e2d6f39f3fb7f6b913ca6
Egor <work.danilov.e@gmail.com>
2025-04-29 00:52:31 +0300

    Pizza.jsx updated

--- src/Components/Pizza.jsx
+++ src/Components/Pizza.jsx
@@ -4,23 +4,30 @@ class Pizza extends React.Component{
     render(){
         return(
             <div className="pizza">
-                <img src="../img/menu/italiaono.png" alt="PizzaName" className="pizza__image" />
-                <h3 className="pizza__name">pizzaName</h3>
-                <p className="desc-text pizza__desc">desc</p>
-                <div className="pizza__sizes">
-                    <ul className="pizza__sizes-list">
-                        <li className="pizza__sizes-item">22</li>
-                        <li className="pizza__sizes-item">28</li>
-                        <li className="pizza__sizes-item">33</li>
-                    </ul>
-                </div>
-                <div className="pizza__add-engridient">+ Добавки</div>
-                <div className="pizza__order">
-                    <div className="pizza__select-order">
-                        <div className="pizza__select-order__cost">700 ₽</div>
-                        <div className="pizza__select-order__count">1</div>
+                <div className="pizza__wrapper">
+                    <img src="https://i.postimg.cc/RZgYcL50/italiaono.png"
+                     alt="PizzaName" className="pizza__image" />
+                    <h3 className="pizza__name">Маргаритта</h3>
+                    <p className="desc-text pizza__desc">Lorem ipsum dolor sit, amet conse adipisicing elit.</p>
+                    <div className="pizza__sizes">
+                        <ul className="pizza__sizes-list">
+                            <li className="pizza__sizes-item">22</li>
+                            <li className="pizza__sizes-item selected">28</li>
+                            <li className="pizza__sizes-item">33</li>
+                        </ul>
                     </div>
-                    <button className="button pizza__order__button">Добавить в корзину</button>
+                    <button className="add-engridient-button">+ Добавки</button>
+                    <div className="pizza__order">
+                        <div className="pizza__select-order">
+                            <div className="pizza__select-order__cost">700 <span>₽</span></div>
+                            <div className="pizza__select-order__count">
+                                <button className="count-button count-munus"></button>
+                                <span className="count">1</span>
+                                <button className="count-button count-plus count-button-active"></button>
+                            </div>
+                        </div>
+                        <button className="button pizza__order__button">Добавить в корзину</button>
+                    </div>
                 </div>
             </div>
         )
