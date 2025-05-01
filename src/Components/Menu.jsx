fba33ff8cc326226c31bc1ea85fed269069252fa
Egor <work.danilov.e@gmail.com>
2025-05-02 00:56:22 +0300

    carousel updated

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -105,11 +105,11 @@ const Menu = (props) => {
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden"></span>
+                        <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden"></span>
+                        <span className="visually-hidden">Next</span>
                     </button>
                 </div>
             </div>
