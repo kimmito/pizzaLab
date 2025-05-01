11033f83be06555564e27acc68fff6bd3aef6744
Egor <work.danilov.e@gmail.com>
2025-05-02 01:14:01 +0300

    images imported

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -65,52 +65,54 @@ const Menu = (props) => {
             </div>
             <div className="bestsellers">
                 <div className="title-text bestsellers__title">Самые популярные</div>
-                <div id="carousel" className="carousel slide">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                <div className="bestsellers-menu">
+                    <div id="carousel" className="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className="bestsellers-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className="bestsellers-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className="bestsellers-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
                     </div>
-                    <div className="carousel-inner">
-                        <div className="carousel-item active">
-                            <div className="bestsellers-menu__slide">
-                                <ul className="menu-list">
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="bestsellers-menu__slide">
-                                <ul className="menu-list">
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div className="bestsellers-menu__slide">
-                                <ul className="menu-list">
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                    <li className="menu-item"><Pizza /></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
         </section>
