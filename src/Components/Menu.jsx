8bb7ceb8c851b84eaa6b826afc1287984bb67426
Egor <work.danilov.e@gmail.com>
2025-05-01 22:27:33 +0300

    carousel.css added

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -15,60 +15,101 @@ const Menu = (props) => {
                 </ul>
             </nav>
             <div className="category-menu">
-                <ul className="menu-list">
-                    <li className="menu-item"><Pizza /></li>
-                    <li className="menu-item"><Pizza /></li>
-                    <li className="menu-item"><Pizza /></li>
-                    <li className="menu-item"><Pizza /></li>
-                </ul>
+                <div id="category-carousel" className="carousel slide">
+                        <div class="carousel-indicators">
+                            <button type="button" data-bs-target="#category-carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#category-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#category-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <div className="category-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className="category-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="carousel-item">
+                                <div className="category-menu__slide">
+                                    <ul className="menu-list">
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                        <li className="menu-item"><Pizza /></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#category-carousel" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden"></span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#category-carousel" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden"></span>
+                        </button>
+                    </div>
             </div>
             <div className="bestsellers">
                 <div className="title-text bestsellers__title">Бестселлеры</div>
                 <div id="carousel" className="carousel slide">
-                <div class="carousel-indicators">
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                </div>
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <div className="bestsellers-menu">
+                            <div className="bestsellers-menu__slide">
                                 <ul className="menu-list">
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="carousel-item">
-                            <div className="bestsellers-menu">
+                            <div className="bestsellers-menu__slide">
                                 <ul className="menu-list">
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
                                 </ul>
                             </div>
                         </div>
                         <div className="carousel-item">
-                            <div className="bestsellers-menu">
+                            <div className="bestsellers-menu__slide">
                                 <ul className="menu-list">
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
-                                    <div className="menu-item"><Pizza /></div>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
+                                    <li className="menu-item"><Pizza /></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
+                        <span className="visually-hidden"></span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
+                        <span className="visually-hidden"></span>
                     </button>
                 </div>
             </div>
