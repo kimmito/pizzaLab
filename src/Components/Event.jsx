9b47e4a063d02ee381b6accef0808b369751ffe6
Egor <work.danilov.e@gmail.com>
2025-05-02 00:39:24 +0300

    event updated

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
+
 const Event = (props) => {
     return(
-        <div className="event">EVENT</div>
+        <div className="event" style={{ backgroundImage: `url(${props.image})`}}>
+            <div className="event-container">
+                <h2 className="event__title">Наш блог</h2>
+                <div className="button event__more-button">Подробнее..</div>
+            </div>
+        </div>
     )
 }
 
