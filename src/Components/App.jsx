93753da18bf7f258af97d2394fe4b450b3cec66e
Egor <work.danilov.e@gmail.com>
2025-05-02 20:05:06 +0300

    footer added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,7 @@ import Home from './Home';
 import Menu from './Menu';
 import Events from './Events';
 import About from './About';
+import Footer from './Footer';
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js";
 class App extends React.Component {
   render() {
@@ -16,6 +17,7 @@ class App extends React.Component {
           <Events />
           <About />
         </main>
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { GiPizzaCutter } from "react-icons/gi";
+import { FaInstagram, FaVk, FaTelegramPlane } from "react-icons/fa";
+
+
+const Footer = (props) => {
+    return(
+        <div className="footer">
+            <div className="footer__container">
+                <div className="footer__nav">
+                    <div className="footer__nav__col">
+                        <a href="#" className="footer__nav__item">
+                            <div className="logo footer__logo">
+                                <GiPizzaCutter className="logo-icon"/> 
+                                <span>Pizza</span><span>LAB</span>
+                            </div>
+                        </a>
+                    </div>
+                    <div className="footer__nav__col">
+                        <a href="#" className="footer__nav__item footer__nav-head">Главная</a>
+                        <a href="#" className="footer__nav__item">Меню</a>
+                        <a href="#" className="footer__nav__item">Бестселлеры</a>
+                        <a href="#" className="footer__nav__item">События</a>
+                        <a href="#" className="footer__nav__item">О нас</a>
+                    </div>
+                    <div className="footer__nav__col">
+                        <a href="#" className="footer__nav__item footer__nav-head">Меню</a>
+                        <a href="#" className="footer__nav__item">Показать все</a>
+                        <a href="#" className="footer__nav__item">Мясные</a>
+                        <a href="#" className="footer__nav__item">Мясные</a>
+                        <a href="#" className="footer__nav__item">Мясные</a>
+                        <a href="#" className="footer__nav__item">Мясные</a>
+                    </div>
+                    <div className="footer__nav__col">
+                        <a href="#" className="footer__nav__item footer__nav-head">События</a>
+                        <a href="#" className="footer__nav__item">Наш блог</a>
+                        <a href="#" className="footer__nav__item">Наш блог</a>
+                        <a href="#" className="footer__nav__item">Наш блог</a>
+                        <a href="#" className="footer__nav__item">Наш блог</a>
+                    </div>
+                    <div className="footer__nav__col">
+                        <a href="#" className="footer__nav__item footer__nav-head">О нас</a>
+                        <a href="#" className="footer__nav__item">Наша история</a>
+                        <a href="#" className="footer__nav__item">Почему мы?</a>
+                    </div>
+                </div>
+        
+                <div className="footer__contacts">
+                    <a href="tel:+7-888-88-88" className="footer__contacts__tel">+7 (888) 888-88-88</a>
+                    <div className="footer__contacts__socials">
+                        <a href="#" className="footer__contacts__socials__link">
+                            <FaInstagram className="social-icon"/>
+                        </a>
+                        <a href="#" className="footer__contacts__socials__link">
+                            <FaVk className="social-icon" />
+                        </a>
+                        <a href="#" className="footer__contacts__socials__link">
+                            <FaTelegramPlane className="social-icon" />
+                            
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+    )
+}
+
+
+export default Footer;
