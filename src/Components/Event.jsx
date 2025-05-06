ac36236e3240e9b65f8d20ad3635f292699bfa6a
Egor <work.danilov.e@gmail.com>
2025-05-06 15:26:24 +0300

    event.jsx updated

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-
 const Event = (props) => {
-    return(
-        <div className="event" style={{ backgroundImage: `url(${props.image})`}}>
+    const { title, background_image, link, desc, active } = props.event || {};
+
+    return (
+        <div className="event" style={{ backgroundImage: background_image ? `url(${background_image})` : "none" }}>
             <div className="event-container">
-                <h2 className="event__title">Наш блог</h2>
-                <div className="button event__more-button">Подробнее..</div>
+                <h2 className="event__title">{title}</h2>
+                {desc ? <p className="desc-text event__desc">{desc}</p> : null}
+                {active ? <a className="button event__button event__more-link" href={link}>Подробнее..</a> : null}
             </div>
         </div>
-    )
+    );
 }
 
 export default Event;
