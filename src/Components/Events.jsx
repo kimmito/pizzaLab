2e011be6dcf8455d1ff4759137fa310035a273d1
Egor <work.danilov.e@gmail.com>
2025-05-06 15:26:13 +0300

    sample-events render added

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -1,41 +1,25 @@
 import React from "react"
 import Event from "./Event";
+import sampleEvents from "./../sample-events"
 
 class Events extends React.Component{
+    state = {
+        events: sampleEvents,
+    }
+
+    renderEvents = () => {
+        return this.state.events.map((event) => (
+            <li key={event.id} className="events__item">
+                <Event event={event} />
+            </li>
+        ));
+    }
     render(){
         return(
             <section className="events" id="events">
-                <div className="events-container">
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <div className="events__item__title">
-                            <h2 className="title-text events-title">События</h2>
-                            <p className="desc-text events-desc">
-                             У нас регулярно проводятся акции, которые позволят вам отведать вкусные блюда по более низкой цене!
-                            </p>
-                        </div>
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                    <div className="events__item">
-                        <Event />
-                    </div>
-                </div>
+                <ul className="events__list">
+                    {this.renderEvents()}
+                </ul>
             </section>
         )
     }
