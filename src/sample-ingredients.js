594685ccff1d2d4a64ca99ae48e3329411312ff2
Egor <work.danilov.e@gmail.com>
2025-05-08 14:51:33 +0300

    sample Ingrediens added

---
+++ src/sample-ingredients.js
@@ -0,0 +1,96 @@
+const sampleIngredients = [
+    {
+        id: 1,
+        name: "Сыр Моцарелла", 
+        price: 70,
+        available: true,
+        image: "",
+        category: "cheese",
+        vegetarian: true
+    },
+    {
+        id: 2,
+        name: "Острая чоризо",
+        price: 95,
+        available: true,
+        image: "",
+        category: "meat",
+        spicyLevel: 2,
+        vegetarian: false
+    },
+    {
+        id: 3,
+        name: "Шампиньоны",
+        price: 60,
+        available: true,
+        image: "",
+        category: "mushrooms",
+        vegetarian: true
+    },
+    {
+        id: 4,
+        name: "Черри томаты",
+        price: 55,
+        available: true,
+        image: "",
+        category: "vegetables",
+        vegetarian: true
+    },
+    {
+        id: 5,
+        name: "Креветки",
+        price: 120,
+        available: true,
+        image: "",
+        category: "seafood",
+        vegetarian: false
+    },
+    {
+        id: 6,
+        name: "Руккола",
+        price: 45,
+        available: true,
+        image: "",
+        category: "greens",
+        vegetarian: true
+    },
+    {
+        id: 7,
+        name: "Пармезан",
+        price: 85,
+        available: true,
+        image: "",
+        category: "cheese",
+        vegetarian: true
+    },
+    {
+        id: 8,
+        name: "Бекон",
+        price: 90,
+        available: true,
+        image: "",
+        category: "meat",
+        vegetarian: false
+    },
+    {
+        id: 9,
+        name: "Артишоки",
+        price: 75,
+        available: false, // временно нет в наличии
+        image: "",
+        category: "vegetables",
+        vegetarian: true
+    },
+    {
+        id: 10,
+        name: "Халапеньо",
+        price: 50,
+        available: true,
+        image: "",
+        category: "vegetables",
+        spicyLevel: 3,
+        vegetarian: true
+    }
+];
+
+export default sampleIngredients;
