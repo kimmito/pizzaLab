f2ed8ae8839c0ca57e68e59a147c59f0faf29b33
Egor <work.danilov.e@gmail.com>
2025-05-15 13:55:17 +0300

    event loading effect deleted

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const Event = (props) => {
     const { title, background_image, link, desc, active } = props.event || {};
-    const [imageLoaded, setImageLoaded] = useState(false);
-
-    useEffect(() => {
-        if (background_image) {
-            const img = new Image();
-            img.src = background_image;
-            img.onload = () => setImageLoaded(true);
-        }
-    }, [background_image]);
 
     return (
-        <div 
-            className={`event ${imageLoaded ? 'event--loaded' : 'event--loading'}`}
-            style={{ backgroundImage: imageLoaded ? `url(${background_image})` : "none" }}
-        >
+        <div>
             <div className="event-container">
                 <h2 className="event__title">{title}</h2>
                 {desc ? <p className="desc-text event__desc">{desc}</p> : null}
