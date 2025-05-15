5e6d128e15be7105b5a548169ac40c70f90da8ff
Egor <work.danilov.e@gmail.com>
2025-05-15 14:23:22 +0300

    Event background render updated

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 const Event = (props) => {
     const { title, background_image, link, desc, active } = props.event || {};
 
     return (
-        <div>
+        <div 
+            className="event"
+            style={{ backgroundImage: `url(${background_image})` }}
+        >
             <div className="event-container">
                 <h2 className="event__title">{title}</h2>
-                {desc ? <p className="desc-text event__desc">{desc}</p> : null}
-                {active ? <a className="button event__button event__more-link" href={link}>Подробнее..</a> : null}
+                {desc && <p className="desc-text event__desc">{desc}</p>}
+                {active && <a className="button event__button event__more-link" href={link}>Подробнее..</a>}
             </div>
         </div>
     );
