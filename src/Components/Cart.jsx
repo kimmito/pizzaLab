b7a9f2bef491a72fbcb9d3b38e425bf39e09e60d
Egor <work.danilov.e@gmail.com>
2025-05-22 23:26:55 +0300

    cart count buttons updated

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -39,7 +39,10 @@ class Cart extends React.Component {
                                                 2: '33 см'
                                             }[item.selectedSize]}
                                         </span>
-                                        <div className="order__item-count"><span className="count-button order__item-count__minus">-</span>{item.count} шт<span className="count-button order__item-count__plus">+</span></div>
+                                        <div className="order__item-count">
+                                            <span className="count-button order__item-count__minus">-</span>
+                                            {item.count} шт
+                                            <span className="count-button order__item-count__plus">+</span></div>
                                         <span className="order__item-price"> {item.totalPrice} ₽</span>
                                     </div>
                                 </div>
