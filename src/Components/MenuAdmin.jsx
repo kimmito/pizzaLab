76247a2d71ef9067dc6c9ea7f75efbd9c2b0a5f7
Egor <work.danilov.e@gmail.com>
2025-05-29 20:46:10 +0300

    switch-case added

--- src/Components/MenuAdmin.jsx
+++ src/Components/MenuAdmin.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import EditMenuForm from "../Components/EditMenuForm"
 import EditEventsForm from "../Components/EditEventsForm"
 import "../css/menuAdmin.css"
+import EditIngredientsForm from "./EditIngredientsFrom"
 
 class MenuAdmin extends React.Component{
     state = {
@@ -21,32 +22,53 @@ class MenuAdmin extends React.Component{
                             <li onClick={() => this.setState({active: "menu"})} className="menu-admin__nav-item">Меню</li>
                             <span className="decorative-line"></span>
                             <li onClick={() => this.setState({active: "events"})} className="menu-admin__nav-item">События</li>
+                            <span className="decorative-line"></span>
+                            <li onClick={() => this.setState({active: "ingredients"})} className="menu-admin__nav-item">Ингредиенты</li>
                         </ul>
                     </nav>
                     <div className="menu-admin__content">
                         <div className="editor">
-                            {this.state.active === "menu" ?        
-                              this.props.menu.map(pizza => (
-                                <EditMenuForm 
-                                  key={pizza.id}
-                                  deleteFromMenu={this.props.deleteFromMenu} 
-                                  updateMenu={this.props.updateMenu} 
-                                  index={pizza.id} 
-                                  pizza={pizza} 
-                                />
-                              ))
-                                : this.state.active === "events" &&
-                                    this.props.events.map(event => (
-                                        <EditEventsForm
-                                            key={event.id}
-                                            index={event.id}
-                                            event={event}
-                                            deleteFromEvents={this.props.deleteFromEvents} 
-                                            updateEvents={this.props.updateEvents} 
-                                        />
-                                    ))
+                            {(() => {
+                                switch (this.state.active) {
+                                    case "menu":
+                                        return this.props.menu.map(pizza => (
+                                            <EditMenuForm 
+                                                key={pizza.id}
+                                                deleteFromMenu={this.props.deleteFromMenu} 
+                                                updateMenu={this.props.updateMenu} 
+                                                index={pizza.id} 
+                                                pizza={pizza} 
+                                            />
+                                        ));
+                                    
+                                    case "events":
+                                        return this.props.events.map(event => (
+                                            <EditEventsForm
+                                                key={event.id}
+                                                index={event.id}
+                                                event={event}
+                                                deleteFromEvents={this.props.deleteFromEvents} 
+                                                updateEvents={this.props.updateEvents} 
+                                            />
+                                        ));
+                                    
+                                    case "ingredients":
+                                      return this.props.ingredients ? 
+                                        this.props.ingredients.map(ingredient => (
+                                          <EditIngredientsForm 
+                                            key={ingredient.id}
+                                            index={ingredient.id}
+                                            ingredient={ingredient}
+                                            deleteFromIngredients={this.props.deleteFromIngredients} 
+                                            updateIngredients={this.props.updateIngredients} 
+                                          />
+                                        )) : 
+                                        <p>Ингредиенты не загружены</p>;
+                                    
+                                    default:
+                                        return null;
                                 }
-                            
+                            })()}
                         </div>
                     </div>
                 </div>
