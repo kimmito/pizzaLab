bad81ef1f059b663dbd9324127332e3cbcc25a56
Egor <work.danilov.e@gmail.com>
2025-06-04 20:33:55 +0300

    env added

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue, off } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGz5IygdttR-xHs9658p7NHtH1rDozFgE",
-  authDomain: "pizzalab-21d41.firebaseapp.com",
-  databaseURL: "https://pizzalab-21d41-default-rtdb.firebaseio.com",
-  projectId: "pizzalab-21d41",
-  storageBucket: "pizzalab-21d41.firebasestorage.app",
-  messagingSenderId: "467931821102",
-  appId: "1:467931821102:web:880a684af7466836871bbf"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
